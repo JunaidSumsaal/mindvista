bd3c2486213089ce098d5a2c4192034288fb96c9
JunaidSumsaal <junaidsumsaal@gmail.com>
2025-03-10 05:35:31 +0300

    added apps

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,15 @@ from two_factor.urls import urlpatterns as tf_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
     path("2fa/", include(tf_urls, namespace="two_factor")),
-    path("", include("landing.urls")),
+    path("", include("landing.urls", namespace="landing")),
+    path("goals/", include("goals.urls", namespace="goals")),
+    path("tasks/", include("task.urls", namespace="task")),
+    path("focus/", include("focus.urls", namespace="focus")),
+    path("notes/", include("notes.urls", namespace="notes")),
+    path("mindmaps/", include("mindmaps.urls", namespace="mindmaps")),
+    path("habits/", include("habits.urls", namespace="habits")),
+    path("reports/", include("reports.urls", namespace="reports")),
+    path("dashboard/", include("dashboard.urls", namespace="dashboard")),
 ]
